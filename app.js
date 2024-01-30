bbdddee3a879ee8f39e687ef9e78e61712ca3f3a
Abhishek soni <soniabhi3568@gmail.com>
2024-01-30 19:36:36 +0530

    formatted the code

--- app.js
+++ app.js
@@ -1,22 +1,20 @@
-import express from 'express'
-import 'dotenv/config'
-import { Dbconnection } from './src/config/db_connection.js'
-import router from './src/router/routes.js';
-import cors from 'cors'
-import bodyParser from 'body-parser';
+import express from "express";
+import "dotenv/config";
+import { Dbconnection } from "./src/config/db_connection.js";
+import router from "./src/router/routes.js";
+import cors from "cors";
+import bodyParser from "body-parser";
 
+Dbconnection();
 
+const app = express(); // calling express js
+app.use(express.json());
+app.use(router); // invoke router
 
-Dbconnection()
+app.use(cors()); // bypass cors policy
+app.use(bodyParser.json()); // parse the data into body
 
-const app = express() // calling express js 
-app.use(express.json())
-app.use(router); // invoke router 
-
-app.use(cors()); // bypass cors policy 
-app.use(bodyParser.json());  // parse the data into body 
-
-var port = process.env.PORT
+var port = process.env.PORT;
 app.listen(port, () => {
-    console.log("server is running at port " + port)
-})
+  console.log("server is running at port " + port);
+});
